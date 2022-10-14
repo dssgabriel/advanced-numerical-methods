8d597208e6c62d4b3e191a14cb003f9a7597e49e
dssgabriel <dss.gabriel@protonmail.com>
2022-10-14 14:44:07 +0200

    Lab 2

---
+++ lab2-3/include/blas.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "matrix.h"
+
+#include <stddef.h>
+
+/**
+ * Computes a double precision scalar-vector product and adds the result to a
+ * vector.
+ *
+ * The `daxpy` routine performs a vector-vector operation defined as:
+ *   y = a * x + y
+ *
+ * Where:
+ * - `a` is a scalar.
+ * - `x` and `y` are vectors.
+ * - `len` is the number of elements in the vectors.
+ **/
+void daxpy(size_t len, double alpha, double const* x, double* y);
+
+/**
+ * Computes a double precision vector-vector dot product.
+ *
+ * The `ddot` routine performs a vector-vector operation defined as:
+ *   res = x * y
+ *
+ * Where:
+ * - `x` and `y` are vectors.
+ * - `len` is the number of elements in the vectors.
+ **/
+double ddot(size_t len, double const* x, double const* y);
+
+/**
+ * Computes the double precision Euclidean/L2 norm of a vector.
+ *
+ * The `nrm2` routine performs a vector reduction operation defined as:
+ *   res = ||x||
+ *
+ * Where:
+ * - `x` is a vector.
+ * - `len` is the number of elements in the vector.
+ **/
+double dnrm2(size_t len, double const* x);
+
+/**
+ * Computes the double precision Frobenius norm of a matrix.
+ *
+ * The `nrmf` routine performs a matrix operation defined as:
+ *   res = ||A||
+ *
+ * Where:
+ * - `A` is a matrix.
+ * - `rows` is the number of elements in the matrix's rows.
+ * - `cols` is the number of elements in the matrix's cols.
+ **/
+double dnrmf(size_t rows, size_t cols, double const* A);
+
+/**
+ * Computes a double precision matrix-vector product and adds the result to the
+ * `y` vector.
+ *
+ * The `dgemv` routine performs a matrix-vector operation defined as:
+ *   y = alpha * A * x + beta * y
+ *
+ * Where:
+ * - `alpha` and `beta` are scalars.
+ * - `x` and `y` are vectors.
+ * - `A` is a matrix.
+ * - `m` is the number of elements in the matrix rows.
+ * - `n` is the number of elements in the matrix columns and in the vectors.
+ **/
+void dgemv(size_t m, size_t n, double alpha, double const* restrict A, double const* restrict x,
+           double beta, double* restrict y);
+
+void classical_gram_schmidt(size_t n, double* restrict x, double* restrict A, size_t deg_m,
+                            matrix_t* mat_Q, matrix_t* mat_H);
+
+void modified_gram_schmidt(size_t n, double* restrict x, double* restrict A, size_t deg_m,
+                           matrix_t* mat_Q, matrix_t* mat_H);
---
+++ lab2-3/src/utils.c
@@ -0,0 +1,72 @@
+#include "utils.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+inline size_t read_user_input()
+{
+    size_t input;
+    scanf("%zu", &input);
+    return input;
+}
+
+inline instant_t instant_now()
+{
+    instant_t self;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &self);
+    return self;
+}
+
+inline double compute_avg_latency(instant_t start, instant_t stop, size_t reps)
+{
+    double s_in_ns = (double)(stop.tv_sec - start.tv_sec) * 1e9;
+    double tot_ns = (stop.tv_nsec - start.tv_nsec) + s_in_ns;
+    return tot_ns / (double)(reps);
+}
+
+inline double rand_double_range(double min, double max)
+{
+    double range = max - min;
+    double div = (double)(RAND_MAX) / (double)(range);
+    return min + random() / div;
+}
+
+void sort_double(double* data, size_t len)
+{
+    for (size_t i = 0; i < len; i++) {
+        for (size_t j = i + 1; j < len; j++) {
+            if (data[i] > data[j]) {
+                double tmp = data[i];
+                data[i] = data[j];
+                data[j] = tmp;
+            }
+        }
+    }
+}
+
+double mean(double const* data, size_t len)
+{
+    double tmp = 0.0;
+    for (size_t i = 0; i < len; i++) {
+        tmp += data[i];
+    }
+    return tmp / (double)(len);
+}
+
+double stddev(double const* data, size_t len)
+{
+    double dev = 0.0;
+    double avg = mean(data, len);
+    for (size_t i = 0; i < len; i++) {
+        dev += (data[i] - avg) * (data[i] - avg);
+    }
+    dev /= (double)(len - 1);
+    return sqrt(dev);
+}
+
+double compute_error(double lhs, double rhs)
+{
+    return fabs((lhs - rhs) / lhs);
+}
